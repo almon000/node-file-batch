8ea1f9425cf555fbae4ad1905d4913466f6e245e
wanjie <almon.wan@brightcells.com>
2016-11-29 13:02:24 +0800

    v0.0.1【finished】遍历，复制，删除函数实现

--- test/unit.js
+++ test/unit.js
@@ -1,8 +1,14 @@
 var fbatch = require('../fbatch')
 
 // 测试 traversal 是否正常工作
-console.log(__dirname)
+console.log('开始遍历文件...')
 fbatch.traversal(__dirname,
   pathname => { console.log('Directory: ', pathname) },
   pathname => { console.log('File:', pathname) })
+console.log('遍历完成')
+
+// 测试 copy 方法
 fbatch.copy('./src', './dest')
+
+// 测试 clear 方法
+fbatch.clear('./dest')
